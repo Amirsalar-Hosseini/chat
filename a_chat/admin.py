1d7019a23f749b1d31d31b381e9f8241ae70b703
amirsalar-hoseini <hosseiniseyedamirsalar@gmail.com>
2025-06-28 13:16:24 +0330

    message and thier group was defined and form logic def was created

--- a_chat/admin.py
+++ a_chat/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from a_chat.models import *
 
-# Register your models here.
+
+admin.site.register(ChatGroup)
+admin.site.register(GroupMessage)
--- a_chat/urls.py
+++ a_chat/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from a_chat.views import *
 
-app_name = 'a_chat'
 urlpatterns = [
-    path('', ChatView.as_view(), name='home'),
+    path('', chat_view, name='home'),
 ]

fb84fea54fc853f30fc9734b7f735493880b785c
amirsalar-hosseini <hosseiniseyedamirsalar@gmail.com>
2025-06-30 16:34:53 +0330

    monitoring and sending any files was defined

--- a_chat/models.py
+++ a_chat/models.py
@@ -1,6 +1,9 @@
+from PIL import Image
 from django.contrib.auth.models import User
 from django.db import models
 import shortuuid
+import os
+
 
 class ChatGroup(models.Model):
     group_name = models.CharField(max_length=120, unique=True, default=shortuuid.uuid)
@@ -19,11 +22,38 @@ class ChatGroup(models.Model):
 class GroupMessage(models.Model):
     group = models.ForeignKey(ChatGroup, on_delete=models.CASCADE, related_name='chat_messages')
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    body = models.CharField(max_length=300)
+    body = models.CharField(max_length=300, null=True, blank=True)
+    file = models.FileField(upload_to='files/', null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
 
+
+    @property
+    def filename(self):
+        if self.file:
+            return os.path.basename(self.file.name)
+        else:
+            return None
+
+
     def __str__(self):
-        return f'{self.author.username} -> {self.body}'
+        if self.body:
+            return f'{self.author.username} -> {self.body}'
+        elif self.file:
+            return f'{self.author.username} -> {self.filename}'
 
     class Meta:
         ordering = ['-created']
+
+
+    @property
+    def is_image(self):
+        # if self.filename.lower().endswith(('.jpg', '.jpeg', '.png', '.gif', '.svg', '.webp')):
+        #     return True
+        # else:
+        #     return False
+        try:
+            image = Image.open(self.file)
+            image.verify()
+            return True
+        except:
+            return False
--- a_chat/views.py
+++ a_chat/views.py
@@ -1,4 +1,6 @@
-from django.http import Http404
+from asgiref.sync import async_to_sync
+from channels.layers import get_channel_layer
+from django.http import Http404, HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
@@ -136,3 +138,25 @@ def chatroom_leave_view(request, chatroom_name):
         messages.success(request, 'You left the Chat.')
         return redirect('home')
     return render(request, 'a_chat/chatroom_leave.html', {'chat_group': chat_group})
+
+
+def chat_file_upload(request, chatroom_name):
+    chat_group = get_object_or_404(ChatGroup, group_name=chatroom_name)
+
+    if request.htmx and request.FILES:
+        file = request.FILES['file']
+        message = GroupMessage.objects.create(
+            file=file,
+            author=request.user,
+            group=chat_group,
+        )
+
+        channel_layer = get_channel_layer()
+        event = {
+            'type': 'message_handler',
+            'message_id': message.id,
+        }
+        async_to_sync(channel_layer.group_send)(
+            chatroom_name, event
+        )
+    return HttpResponse()
